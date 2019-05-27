514ad139a338edfb5231cb424df69581def9e0cc
Alisher Sadikov <asadikov@msts.com>
2019-05-27 12:18:42 -0500

    Create doctors#show and rework the views

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -4,6 +4,11 @@ class DoctorsController < ApplicationController
   end
 
   def search
+    # Rendering a view from a post route action seems really bad and non-restful
     @doctors = DoctorSearchService.process(name: params[:name])
+  end
+
+  def show
+    @doctor = Doctor.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'doctors#index'
 
-  resources :doctors, only: [:index]
+  resources :doctors, only: [:index, :show]
   # route for search endpoint
   post 'search' => 'doctors#search'
 
