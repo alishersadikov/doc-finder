d55547f20946c9d303c07b4b361710a80b1e613d
Alisher Sadikov <asadikov@msts.com>
2019-05-27 12:16:48 -0500

    Turn doctor PORO into doctor presenter

--- test/models/doctor_presenter_test.rb
+++ test/models/doctor_presenter_test.rb
@@ -1,4 +1,4 @@
-class DoctorSearchServiceTest < ActiveSupport::TestCase
+class DoctorPresenterTest < ActiveSupport::TestCase
   def sample_response
     read_file('sample_search_response.json')
   end
@@ -8,7 +8,7 @@ class DoctorSearchServiceTest < ActiveSupport::TestCase
   end
 
   def sample_doctor
-    Doctor.new(data: sample_doctor_data)
+    DoctorPresenter.new(data: sample_doctor_data)
   end
 
   test 'name' do
@@ -29,17 +29,5 @@ class DoctorSearchServiceTest < ActiveSupport::TestCase
 
   test 'primary_care_physician' do
     assert_not sample_doctor.primary_care_physician
-  end
-
-  test 'can be easily converted to JSON' do
-    expected = {
-      name: sample_doctor.name,
-      title: sample_doctor.title,
-      specialties: sample_doctor.specialties,
-      insurances: sample_doctor.insurances,
-      primary_care_physician: sample_doctor.primary_care_physician
-    }.to_json
-
-    assert_equal expected, sample_doctor.to_json
   end
 end
